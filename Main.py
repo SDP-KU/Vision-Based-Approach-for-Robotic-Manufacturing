2d6ebf37d778acf2f45f58fedbdffa17719c0f91
Ameera0AA <87200576+Ameera0AA@users.noreply.github.com>
2021-07-13 15:32:10 +0400

    Update Main.py
    New Camera Matrix.
    Adding Drill Function.
    Initial Translation Motion from Camera to Drill.

--- Main.py
+++ Main.py
@@ -2,30 +2,30 @@ from robolink import *    # RoboDK API
 from robodk import *      # Robot toolbox
 import numpy as np
 import sys, time, math, cv2, keyboard
-import matplotlib.pyplot as plt #library for plotting and visualization
 import pyrealsense2 as rs
 from scipy.spatial.transform import Rotation as R
 import cv2.aruco as aruco
 from Detect_Holes_Function import HoleDetec
 from ArUco_Detect import ArUco
 from Move_to_Hole import Move
+from Drill import Drill_on
 
 ## USER DEFINED
 # camera position with respect to end-effector 
-C2E_x = -0.0597*1000
-C2E_y = -0.0373*1000
-C2E_z = 0.1996*1000
-C2E_rx = np.multiply(-1.5702158, 180/math.pi)
-C2E_ry = np.multiply(0.0320472, 180/math.pi)
-C2E_rz = np.multiply(-2.3870739, 180/math.pi)
+C2E_x = -0.0689*1000
+C2E_y = -0.1042*1000
+C2E_z = 0.2140*1000
+C2E_rx = np.multiply(-1.6229758, 180/math.pi) 
+C2E_ry = np.multiply(-0.0024435, 180/math.pi)
+C2E_rz = np.multiply(2.38062460, 180/math.pi)
 
 # Drill position with respect to end-effector
 D2E_x = 0
 D2E_y = 0
 D2E_z = 150
-D2E_rx = np.multiply(-1.5702158, 180/math.pi)
-D2E_ry = np.multiply(0.0320472, 180/math.pi)
-D2E_rz = np.multiply(-2.3870739, 180/math.pi)
+D2E_rx = np.multiply(-1.6229758, 180/math.pi) 
+D2E_ry = np.multiply(-0.0024435, 180/math.pi)
+D2E_rz = np.multiply(2.38062460, 180/math.pi)
 
 # establish a link with the simulator
 RDK = Robolink()
@@ -99,31 +99,39 @@ def CreateHole( name, pos, x,y,z ):
 
 # Create and initialize Holes, return Hole pose
 pose1 = CreateHole("Hole 1", aruco_pose , -70, 35, 0)
-pose2 = CreateHole("Hole 2", pose1, -50, 0, 0)
-pose3 = CreateHole("Hole 3", pose2, -50, 0, 0)
-pose4 = CreateHole("Hole 4", pose3, -50, 0, 0)
-pose5 = CreateHole("Hole 5", pose4, 0, 45, 0)
-pose6 = CreateHole("Hole 6", pose5, 50, 0, 0)
-pose7 = CreateHole("Hole 7", pose6, 50, 0, 0)
-pose8 = CreateHole("Hole 8", pose7, 50, 0, 0)
-pose9 = CreateHole("Hole 9", pose8, 50, 0, 0)
-pose10 = CreateHole("Hole 10", pose9, 50, 0, 0)
-pose11 = CreateHole("Hole 11", pose10, 50, 0, 0)
-pose12 = CreateHole("Hole 12", pose11, 50, 0, 0)
-pose13 = CreateHole("Hole 13", pose12, 50, 0, 0)
+# pose2 = CreateHole("Hole 2", pose1, -50, 0, 0)
+# pose3 = CreateHole("Hole 3", pose2, -50, 0, 0)
+# pose4 = CreateHole("Hole 4", pose3, -50, 0, 0)
+# pose5 = CreateHole("Hole 5", pose4, 0, 45, 0)
+# pose6 = CreateHole("Hole 6", pose5, 50, 0, 0)
+# pose7 = CreateHole("Hole 7", pose6, 50, 0, 0)
+# pose8 = CreateHole("Hole 8", pose7, 50, 0, 0)
+# pose9 = CreateHole("Hole 9", pose8, 50, 0, 0)
+# pose10 = CreateHole("Hole 10", pose9, 50, 0, 0)
+# pose11 = CreateHole("Hole 11", pose10, 50, 0, 0)
+# pose12 = CreateHole("Hole 12", pose11, 50, 0, 0)
+# pose13 = CreateHole("Hole 13", pose12, 50, 0, 0)
 
 # Move to Target
 Move(robot, aruco_pose, "aruco", ENDEFFECTOR_TO_CAM) # Move to ArUco
 Move(robot, pose1, "hole 1", ENDEFFECTOR_TO_CAM) # Move to Hole 1
-Move(robot, pose2, "hole 2", ENDEFFECTOR_TO_CAM) # Move to Hole 2
-Move(robot, pose3, "hole 3", ENDEFFECTOR_TO_CAM) # Move to Hole 3
-Move(robot, pose4, "hole 4", ENDEFFECTOR_TO_CAM) # Move to Hole 4
-Move(robot, pose5, "hole 5", ENDEFFECTOR_TO_CAM) # Move to Hole 5
-Move(robot, pose6, "hole 6", ENDEFFECTOR_TO_CAM) # Move to Hole 6
-Move(robot, pose7, "hole 7", ENDEFFECTOR_TO_CAM) # Move to Hole 7
-Move(robot, pose8, "hole 8", ENDEFFECTOR_TO_CAM) # Move to Hole 8
-Move(robot, pose9, "hole 9", ENDEFFECTOR_TO_CAM) # Move to Hole 9
-Move(robot, pose10, "hole 10", ENDEFFECTOR_TO_CAM) # Move to Hole 10
-Move(robot, pose11, "hole 11", ENDEFFECTOR_TO_CAM) # Move to Hole 11
-Move(robot, pose12, "hole 12", ENDEFFECTOR_TO_CAM) # Move to Hole 12
-Move(robot, pose13, "hole 13", ENDEFFECTOR_TO_CAM) # Move to Hole 13
+
+input("press enter to proceed")
+robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_CAM*transl(-40,-70,20)) # approximate value
+input("again, press enter to proceed")
+Drill_on()
+
+# Hole Position must change again !!!
+
+# Move(robot, pose2, "hole 2", ENDEFFECTOR_TO_CAM) # Move to Hole 2
+# Move(robot, pose3, "hole 3", ENDEFFECTOR_TO_CAM) # Move to Hole 3
+# Move(robot, pose4, "hole 4", ENDEFFECTOR_TO_CAM) # Move to Hole 4
+# Move(robot, pose5, "hole 5", ENDEFFECTOR_TO_CAM) # Move to Hole 5
+# Move(robot, pose6, "hole 6", ENDEFFECTOR_TO_CAM) # Move to Hole 6
+# Move(robot, pose7, "hole 7", ENDEFFECTOR_TO_CAM) # Move to Hole 7
+# Move(robot, pose8, "hole 8", ENDEFFECTOR_TO_CAM) # Move to Hole 8
+# Move(robot, pose9, "hole 9", ENDEFFECTOR_TO_CAM) # Move to Hole 9
+# Move(robot, pose10, "hole 10", ENDEFFECTOR_TO_CAM) # Move to Hole 10
+# Move(robot, pose11, "hole 11", ENDEFFECTOR_TO_CAM) # Move to Hole 11
+# Move(robot, pose12, "hole 12", ENDEFFECTOR_TO_CAM) # Move to Hole 12
+# Move(robot, pose13, "hole 13", ENDEFFECTOR_TO_CAM) # Move to Hole 13
