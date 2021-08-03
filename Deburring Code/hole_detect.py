cb8616944e55c617051cc79465e686f4973464ac
Ameera0AA <87200576+Ameera0AA@users.noreply.github.com>
2021-08-03 15:45:04 +0400

    Update hole_detect.py
    new function 'operation_detect' to be used in operation_hole in operation_zero

--- Deburring Code/hole_detect.py
+++ Deburring Code/hole_detect.py
@@ -50,7 +50,7 @@ def HoleDetec():
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         # Set the lower and upper HSV range according to the value selected
         # by the trackbar
-        lower_range = np.array([0, 170, 0]) # [0, 170, 0]
+        lower_range = np.array([0, 145, 0]) # [0, 170, 0]
         upper_range = np.array([179, 255, 255])
         
         # Filter the image and get the binary mask, where white represents 
@@ -89,8 +89,8 @@ def HoleDetec():
                 x_v.append(int(k.pt[0]))
                 y_v.append(int(k.pt[1]))
             cv2.addWeighted(frame, opacity, im, 1 - opacity, 0, im)
-            cv2.imshow("Output", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            cv2.waitKey(1) # & 0xff
+            cv2.imshow("Output", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) 
+            cv2.waitKey(1) & 0xff
             # i = 1
             # cv2.imwrite(str(path+"/"+str(i)+".jpg"), cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         
@@ -274,3 +274,76 @@ def HoleDetec_Manual():
     return (center_x,center_y)
 
 # a,b = HoleDetec_Manual()
+
+def operation_detect():
+    pipeline = rs.pipeline()
+    config = rs.config()
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+    color_profile = rs.video_stream_profile(pipeline_profile.get_stream(rs.stream.color))
+    color_intrinsics = color_profile.get_intrinsics()
+    w, h = color_intrinsics.width, color_intrinsics.height
+    found_rgb = False   #Intial Value
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+    pipeline.start(config)
+    x_v = []
+    y_v = []
+    center_x, center_y = 0, 0
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        if  not color_frame:
+            continue
+        frame  = np.asanyarray(color_frame.get_data())
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_range = np.array([0, 190, 0]) 
+        upper_range = np.array([179, 255, 255])
+        mask = cv2.inRange(hsv, lower_range, upper_range)
+        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        detector = cv2.SimpleBlobDetector_create()
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByArea = True
+        params.minArea = 200 # 150
+        params.maxArea = 3000 # 4000
+        params.filterByCircularity = True
+        params.minCircularity = 0.1
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.1
+        params.minDistBetweenBlobs = 100
+        opacity = 0.1
+        detector = cv2.SimpleBlobDetector_create(params)
+        im = np.asanyarray(mask_3)
+        keypoints = detector.detect(im)
+        if keypoints is not None:
+            for k in keypoints:
+                cv2.circle(frame, (int(k.pt[0]), int(k.pt[1])), int(k.size/3), (0, 0, 255), -1)
+                cv2.line(frame, (int(k.pt[0])-20, int(k.pt[1])), (int(k.pt[0])+20, int(k.pt[1])), (0,0,0), 2)
+                cv2.line(frame, (int(k.pt[0]), int(k.pt[1])-20), (int(k.pt[0]), int(k.pt[1])+20), (0,0,0), 2)
+                x_v.append(int(k.pt[0]))
+                y_v.append(int(k.pt[1]))
+            cv2.addWeighted(frame, opacity, im, 1 - opacity, 0, im)
+            cv2.imshow("Output", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) 
+            cv2.waitKey(1)  & 0xff
+        px, py = 320, 246
+        p = math.sqrt((px*px) + (py*py))
+        b = 100
+        for x,y in zip(x_v,y_v):
+            xy = math.sqrt((x*x) + (y*y))
+            if abs(xy - p) <= b:
+                center_x, center_y = x, y
+                b = abs(xy - p)
+        if center_x == 0 and center_y == 0:
+            continue
+        else:
+            print("Closest Circle is:", center_x,",", center_y)
+            print ("Differenace with the optical center: ", (px-center_x),",",(py-center_y))
+            break
+    return (center_x,center_y)
