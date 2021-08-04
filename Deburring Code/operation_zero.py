f4a7cb02d1d848b4bfa0272b2e04a4bbf9c74e7b
Ameera0AA <87200576+Ameera0AA@users.noreply.github.com>
2021-08-04 21:22:59 +0400

    Update operation_zero.py

--- Deburring Code/operation_zero.py
+++ Deburring Code/operation_zero.py
@@ -7,9 +7,7 @@ from scipy.spatial.transform import Rotation as R
 import cv2.aruco as aruco
 from ArUco_detect import ArUco
 from move_to_hole import Move
-from operation_hole import operation, force_sense, Drill_on
-
-
+from operation_hole import operation_correct, operation_force, operation_drill
 
 ## USER DEFINED
 # camera position with respect to end-effector 
@@ -21,13 +19,12 @@ C2E_ry = np.multiply(-0.0024435, 180/math.pi)
 C2E_rz = np.multiply(2.38062460, 180/math.pi)
 
 # Drill position with respect to end-effector
-D2E_x = -121.1162
-D2E_y = -135.2056
-D2E_z = 144.74650
-D2E_rx = np.multiply(1.55860250, 180/math.pi) 
-D2E_ry = np.multiply(-0.00987130, 180/math.pi)
-D2E_rz = np.multiply(-0.67725880, 180/math.pi)
-D2E_rad = [D2E_x/1000, D2E_y/1000, D2E_z/1000, 1.55860250, -0.00987130, -0.67725880]
+D2E_x = -138.2393
+D2E_y = -124.7527
+D2E_z = 141.2898
+D2E_rx = np.multiply(1.5750029, 180/math.pi) 
+D2E_ry = np.multiply(0.0040261, 180/math.pi)
+D2E_rz = np.multiply(-0.7659436, 180/math.pi)
 
 # establish a link with the simulator
 RDK = Robolink()
@@ -37,19 +34,14 @@ robot = RDK.Item('UR10 A', ITEM_TYPE_ROBOT)
 def connect_DK():
     # Connect To Robot
     RUN_ON_ROBOT = True
-    # if RDK.RunMode() != RUNMODE_SIMULATE:
-    #     RUN_ON_ROBOT = False
     if RUN_ON_ROBOT:
-        # robot.setConnectionParams('192.168.50.110',30000,'/', 'anonymous','') # NEW LINE
         # Connect to the robot using default IP
-        # success = robot.Connect() # Try to connect once
-        success = robot.ConnectSafe() # Try to connect multiple times ## NEW LINE
+        success = robot.ConnectSafe() # Try to connect multiple times
         status, status_msg = robot.ConnectedState()
         if status != ROBOTCOM_READY:
             # Stop if the connection did not succeed
             print(status_msg)
             raise Exception("Failed to connect: " + status_msg)
-        
         # This will set to run the API programs on the robot and the simulator (online programming)
         RDK.setRunMode(RUNMODE_RUN_ROBOT)
         # Note: This is set automatically when we Connect() to the robot through the API
@@ -66,13 +58,12 @@ robot_config = robot.JointsConfig(robot_joints)
 # ArUco Detection
 aruco_pos = ArUco()
 
-# Get the position matrix of the marker
+# Get the position matrix of the marker 4x4
 ENDEFFECTOR_TO_CAM = robodk.KUKA_2_Pose([C2E_x,C2E_y,C2E_z,C2E_rz,C2E_ry,C2E_rx])
-ENDEFFECTOR_TO_DRILL= robodk.KUKA_2_Pose([D2E_x, D2E_y, D2E_z, D2E_rz,D2E_ry,D2E_rx])
+ENDEFFECTOR_TO_DRILL= robodk.KUKA_2_Pose([D2E_x, D2E_y, D2E_z, D2E_rz, D2E_ry, D2E_rx])
 
-# add tool (camera)
-Drill = robot.AddTool(ENDEFFECTOR_TO_DRILL, tool_name='Rivet') 
-Camera = robot.AddTool(ENDEFFECTOR_TO_CAM, tool_name='Camera')
+Drill = robot.AddTool(ENDEFFECTOR_TO_DRILL, tool_name='Drill') # Add Drill
+Camera = robot.AddTool(ENDEFFECTOR_TO_CAM, tool_name='Camera') # Add Camera
 robot.setTool(Camera) # Set Active Tool
 
 table = RDK.Item('Table') # Add Table Item
@@ -88,11 +79,9 @@ new_robot_joints = robot.SolveIK(aruco_pose)
 new_robot_config = robot.JointsConfig(new_robot_joints)
 
 def speed():
-    robot.setSpeed(speed_linear =20, speed_joints=3, accel_linear=-1, accel_joints=-1) # set speed
+    robot.setSpeed(speed_linear =20, speed_joints=3, accel_linear=-1, accel_joints=-1)
 
 speed() # Set Speed
-
-# RDK.setCollisionActive(COLLISION_ON if CHECK_COLLISIONS else COLLISION_OFF) # collision 
 
 def CreateHole( name, pos, x,y,z ):
     Hole_Item = RDK.Item(name, ITEM_TYPE_TARGET)        # Add Item
@@ -104,52 +93,59 @@ def CreateHole( name, pos, x,y,z ):
     return (Hole_Pose)
 
 # Create and initialize Holes, return Hole pose
-pose1 = CreateHole("Hole 1", aruco_pose , -70, 80, 0) # -70, 35, 0
+pose1 = CreateHole("Hole 1", aruco_pose , -70, 30, 0)
 pose2 = CreateHole("Hole 2", pose1, -50, 0, 0)
 pose3 = CreateHole("Hole 3", pose2, -50, 0, 0)
-# pose4 = CreateHole("Hole 4", pose3, -50, 0, 0)
-# pose5 = CreateHole("Hole 5", pose4, 0, 45, 0)
-# pose6 = CreateHole("Hole 6", pose5, 50, 0, 0)
-# pose7 = CreateHole("Hole 7", pose6, 50, 0, 0)
-# pose8 = CreateHole("Hole 8", pose7, 50, 0, 0)
-# pose9 = CreateHole("Hole 9", pose8, 50, 0, 0)
-# pose10 = CreateHole("Hole 10", pose9, 50, 0, 0)
-# pose11 = CreateHole("Hole 11", pose10, 50, 0, 0)
-# pose12 = CreateHole("Hole 12", pose11, 50, 0, 0)
-# pose13 = CreateHole("Hole 13", pose12, 50, 0, 0)
+pose4 = CreateHole("Hole 4", pose3, -50, 0, 0)
+pose5 = CreateHole("Hole 5", pose4, 0, 40, 0)
+pose6 = CreateHole("Hole 6", pose5, 50, 0, 0)
+pose7 = CreateHole("Hole 7", pose6, 50, 0, 0)
+pose8 = CreateHole("Hole 8", pose7, 50, 0, 0)
+pose9 = CreateHole("Hole 9", pose8, 50, 0, 0)
+pose10 = CreateHole("Hole 10", pose9, 50, 0, 0)
+pose11 = CreateHole("Hole 11", pose10, 50, 0, 0)
+pose12 = CreateHole("Hole 12", pose11, 50, 0, 0)
+pose13 = CreateHole("Hole 13", pose12, 50, 0, 0)
 
-# Move to ArUco
-Move(robot, aruco_pose, "aruco", ENDEFFECTOR_TO_CAM) # Move to ArUco
+robot.MoveL(aruco_pose * transl(0,0,-125)) # Move to ArUco 
 
-def move_hole(pose, char):
-    Move(robot, pose, "aruco", ENDEFFECTOR_TO_CAM) # Move to Hole
-    operation() # hole detection
+def operation(pose):
+    robot.MoveL(pose * transl(0,0,-125))
+    # Move(robot, pose, "aruco", ENDEFFECTOR_TO_CAM) # Move to Hole
+    operation_correct() # hole detection
     connect_DK() # Re-Connect with the Robot
     speed() # Re-set Speed
     robot.setTool(Drill) # Set active tool to Drill
-    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(31,74,27)) # Move Drill to infront of the Hole, Move to x,y position of the Camera, move forward in the z
-    input("enter")
-    force_sense() # insert and force sensing
-    input("re-connect")
+    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(31,78,25)) # Move Drill to infront of the Hole, Move to x,y position of the Camera, move forward in the z
+    operation_force() # insert and force sensing
     connect_DK() # Re-Connect with the Robot
     speed() # Re-set Speed
     input("enter")
-    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-32)) # Move the Drill out until the Debarring tip is around the walls of the Hole (REAR)
+    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-33)) # Move the Drill out until the Debarring tip is around the walls of the Hole (REAR)
     input("enter")
-    Drill_on() # Strat Debarring
+    operation_drill() # Strat Debarring
     connect_DK() # Re-Connect with the Robot
     speed() # Re-set Speed
     input("enter")
-    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-8)) # Move the Drill out until the Debarring tip is around the walls of the Hole (FRONT)
+    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-9)) # Move the Drill out until the Debarring tip is around the walls of the Hole (FRONT)
     input("enter")
-    Drill_on() # Strat Debarring
+    operation_drill() # Strat Debarring
     connect_DK() # Re-Connect with the Robot
     speed() # Re-set Speed
-    input("enter")
-    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-18)) # Take Drill out of the Hole
-    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(-31,-74,-27)) # Put Camera infront of the Hole again
+    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(0,0,-24)) # Take Drill out of the Hole
+    robot.MoveL(robot.SolveFK(robot.Joints())*ENDEFFECTOR_TO_DRILL*transl(-31,-78,-25)) # Put Camera infront of the Hole again
     robot.setTool(Camera) # Re-set acvtive tool to Camera
 
-move_hole(pose1, "hole 1")
-move_hole(pose2, "hole 2")
-move_hole(pose3, "hole 3")
+operation(pose1)
+operation(pose2)
+operation(pose3)
+operation(pose4)
+operation(pose5)
+operation(pose6)
+operation(pose7)
+operation(pose8)
+operation(pose9)
+operation(pose10)
+operation(pose11)
+operation(pose12)
+operation(pose13)
