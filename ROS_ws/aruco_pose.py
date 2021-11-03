0f34de8be2a51b24fbc9e56c8165f6b905a3f927
bushraDh <87926300+bushraDh@users.noreply.github.com>
2021-11-03 12:32:08 +0400

    Update aruco_pose.py

--- ROS_ws/aruco_pose.py
+++ ROS_ws/aruco_pose.py
@@ -4,7 +4,7 @@
 from numpy.lib.function_base import vectorize
 import roslib
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import Pose
 # numpy and scipy
 import numpy as np
@@ -24,40 +24,48 @@ from sensor_msgs.msg import CompressedImage
 
 bridge = CvBridge()
 
-#Intrensic Parameters
-Cameramatrix=np.array([[611.301, 0, 320.367],[0, 611.506, 246.129],[0,0,1]])
-#Find RGB 
+cameraM = np.array([])
+cameradist=np.array([0.,    0.,   0.,   0.,    0.])
+def Cameramatrix(d):
+    #Intrensic Parameters
+    Cameramatrix=np.array([[d.K[0], d.K[1],d.K[2]],
+                  [d.K[3], d.K[4],d.K[5]],
+                  [d.K[7],d.K[6],d.K[8]]])
+  #  Cameramatrix=np.array([[611.301, 0, 320.367],[0, 611.506, 246.129],[0,0,1]])
+    cameradist1=np.array([d.D[0],d.D[1],d.D[2],d.D[3]])
+    global cameraM
+    cameraM=Cameramatrix
+    global cameradist
+    cameradist = cameradist1
 
 # ArUco Detection Code
 # Start streaming
 
 #---Parametrs 
-markersize= 9 #[cm]
+markersize= 7.5 #[cm]
 #---camera 
-cameradist=np.array([0.,    0.,   0.,   0.,    0.])
+
 
 #---Aruco Dictionary
 arucodic=aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
 parameters=aruco.DetectorParameters_create()
 #  parameters.cornerRefinementMethod = aruco.CORNER_REFINE_SUBPIX
-parameters.cornerRefinementWinSize = 5
-parameters.cornerRefinementMinAccuracy = 0.001
-parameters.cornerRefinementMaxIterations = 5
+
 
 # publishing the pose
 pub = rospy.Publisher('pose', Pose, queue_size=1)
 
 def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     color_image= bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+    h1, w1, _ = np.shape(color_image)
     #--Convert to gray scale
     gray=cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY) # put the saved pic
     #---Find aruco markers
     corners, ids, rejected=  aruco.detectMarkers(image=gray, dictionary=arucodic, parameters=parameters)
-    if ids != None :
+    if (ids != None) & (cameraM !=[]):
         # the following line is not right 
-        ret= aruco.estimatePoseSingleMarkers(corners,markersize, Cameramatrix, cameradist)
+        ret= aruco.estimatePoseSingleMarkers(corners,markersize, cameraM, cameradist)
         #---unpack output
         rvec = ret[0][0][0]
         tvec = ret[1][0][0]
@@ -65,17 +73,17 @@ def callback(data):
         # Convert it back to vector
         tvec = np.multiply(tvec, 10) # cm 2 mm
         rotation = R.from_rotvec(rvec)
-        print(rvec)
+        # print(rvec)
         rotation_M=rotation.as_dcm()
         R0=np.array([[1.,0.,0.],[0.,-1.,0.],[0.,0.,-1.]])
-        R1_M=np.matmul(R0, rotation_M)
-        print("A")
-        print(rotation_M)
-        print(R0)
-        print(R1_M)
+        R1_M=np.matmul(rotation_M, R0)
+        # print("A")
+        # print(rotation_M)
+        # print(R0)
+        # print(R1_M)
         R1=R.from_dcm(R1_M)
         quat = R1.as_quat()
-        print(quat)
+        # print(quat)
 
         p = Pose()
         p.position.x = tvec[0]
@@ -90,8 +98,8 @@ def callback(data):
 
 def PicToPose():
         rospy.init_node('PicToPose', anonymous=True)
+        rospy.Subscriber("/camera/color/camera_info",CameraInfo, Cameramatrix)
         rospy.Subscriber("/camera/color/image_raw",Image, callback)
-        
         rospy.spin()
 
 
@@ -99,4 +107,4 @@ if __name__ == '__main__':
     try:
         PicToPose()
     except rospy.ROSInterruptException:
-        pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        pass    
