cca649f475e8ee3011520b0a27485b2b8a472bb5
bushraDh <87926300+bushraDh@users.noreply.github.com>
2021-11-08 14:52:28 +0400

    Update aruco_pose.py

--- ROS_ws/aruco_pose.py
+++ ROS_ws/aruco_pose.py
@@ -14,13 +14,9 @@ import cv2
 from cv2 import aruco
 from cv_bridge import CvBridge
 # Python libs
-import sys, time, math
-import math
 from scipy.spatial.transform import Rotation as R
 from cv_bridge import CvBridge
 
-# Ros Messages
-from sensor_msgs.msg import CompressedImage
 
 bridge = CvBridge()
 
@@ -31,20 +27,15 @@ def Cameramatrix(d):
     Cameramatrix=np.array([[d.K[0], d.K[1],d.K[2]],
                   [d.K[3], d.K[4],d.K[5]],
                   [d.K[7],d.K[6],d.K[8]]])
-  #  Cameramatrix=np.array([[611.301, 0, 320.367],[0, 611.506, 246.129],[0,0,1]])
     cameradist1=np.array([d.D[0],d.D[1],d.D[2],d.D[3]])
     global cameraM
     cameraM=Cameramatrix
     global cameradist
     cameradist = cameradist1
 
-# ArUco Detection Code
-# Start streaming
-
 #---Parametrs 
 markersize= 7.5 #[cm]
 #---camera 
-
 
 #---Aruco Dictionary
 arucodic=aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
@@ -63,28 +54,20 @@ def callback(data):
     gray=cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY) # put the saved pic
     #---Find aruco markers
     corners, ids, rejected=  aruco.detectMarkers(image=gray, dictionary=arucodic, parameters=parameters)
-    if (ids != None) & (cameraM !=[]):
-        # the following line is not right 
+    if (ids != None) and (cameraM !=[]):
         ret= aruco.estimatePoseSingleMarkers(corners,markersize, cameraM, cameradist)
-        #---unpack output
+        #unpack output
         rvec = ret[0][0][0]
         tvec = ret[1][0][0]
     
         # Convert it back to vector
-        tvec = np.multiply(tvec, 10) # cm 2 mm
+        tvec = np.multiply(tvec, 10) # cm to mm
         rotation = R.from_rotvec(rvec)
-        # print(rvec)
         rotation_M=rotation.as_dcm()
         R0=np.array([[1.,0.,0.],[0.,-1.,0.],[0.,0.,-1.]])
         R1_M=np.matmul(rotation_M, R0)
-        # print("A")
-        # print(rotation_M)
-        # print(R0)
-        # print(R1_M)
         R1=R.from_dcm(R1_M)
         quat = R1.as_quat()
-        # print(quat)
-
         p = Pose()
         p.position.x = tvec[0]
         p.position.y = tvec[1]
@@ -107,4 +90,4 @@ if __name__ == '__main__':
     try:
         PicToPose()
     except rospy.ROSInterruptException:
-        pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
