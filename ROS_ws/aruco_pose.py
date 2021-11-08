eff01b57be1d417542bc8ea73452fbf4e0768cb5
bushraDh <87926300+bushraDh@users.noreply.github.com>
2021-11-08 14:41:53 +0400

    Update aruco_pose.py

--- ROS_ws/aruco_pose.py
+++ ROS_ws/aruco_pose.py
@@ -107,4 +107,4 @@ if __name__ == '__main__':
     try:
         PicToPose()
     except rospy.ROSInterruptException:
-        pass    
+        pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
